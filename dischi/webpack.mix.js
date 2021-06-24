e5a624acf9006f2495cdb04b4612e41ee2c60ebe
valeriomatranga <78118886+valeriomatranga@users.noreply.github.com>
2021-06-24 15:28:18 +0200

    collegamenti di base

---
+++ dischi/webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+
+mix
+.js('src/app.js', 'js')
+.sass('src/app.scss', 'css')
+.options({processCssUrls:false})
+.copyDirectory('src/img', 'assets/img')
+.setPublicPath('assets');
